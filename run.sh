b64ccdf40a823f8207f3c92327e49536523dfb9a
chen <4199191@qq.com>
2019-06-19 11:12:00 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -42,42 +42,42 @@ echo 'alias vi=vim' >> ~/.zshrc
 echo 'export EDITOR=/usr/bin/vim' >> ~/.zshrc
 echo 'export VISUAL=/usr/bin/vim' >> ~/.zshrc
 
- echo '
- # key bindings
- bindkey "\e[1~" beginning-of-line
- bindkey "\e[4~" end-of-line
- bindkey "\e[5~" beginning-of-history
- bindkey "\e[6~" end-of-history
+echo '
+# key bindings
+bindkey "\e[1~" beginning-of-line
+bindkey "\e[4~" end-of-line
+bindkey "\e[5~" beginning-of-history
+bindkey "\e[6~" end-of-history
  
- # for rxvt
- bindkey "\e[8~" end-of-line
- bindkey "\e[7~" beginning-of-line
- # for non RH/Debian xterm, cannot hurt for RH/DEbian xterm
- bindkey "\eOH" beginning-of-line
- bindkey "\eOF" end-of-line
- # for freebsd console
- bindkey "\e[H" beginning-of-line
- bindkey "\e[F" end-of-line
- # completion in the middle of a line
- bindkey "^i" expand-or-complete-prefix
+# for rxvt
+bindkey "\e[8~" end-of-line
+bindkey "\e[7~" beginning-of-line
+# for non RH/Debian xterm, cannot hurt for RH/DEbian xterm
+bindkey "\eOH" beginning-of-line
+bindkey "\eOF" end-of-line
+# for freebsd console
+bindkey "\e[H" beginning-of-line
+bindkey "\e[F" end-of-line
+# completion in the middle of a line
+bindkey "^i" expand-or-complete-prefix
  
- bindkey -s "^[Op" "0"
- bindkey -s "^[On" "."
- bindkey -s "^[OM" "^M"
- bindkey -s "^[Oq" "1"
- bindkey -s "^[Or" "2"
- bindkey -s "^[Os" "3"
- bindkey -s "^[Ot" "4"
- bindkey -s "^[Ou" "5"
- bindkey -s "^[Ov" "6"
- bindkey -s "^[Ow" "7"
- bindkey -s "^[Ox" "8"
- bindkey -s "^[Oy" "9"
- bindkey -s "^[Ol" "+"
- bindkey -s "^[Om" "-"
- bindkey -s "^[Oj" "*"
- bindkey -s "^[Oo" "/"
- ' >> ~/.zshrc
+bindkey -s "^[Op" "0"
+bindkey -s "^[On" "."
+bindkey -s "^[OM" "^M"
+bindkey -s "^[Oq" "1"
+bindkey -s "^[Or" "2"
+bindkey -s "^[Os" "3"
+bindkey -s "^[Ot" "4"
+bindkey -s "^[Ou" "5"
+bindkey -s "^[Ov" "6"
+bindkey -s "^[Ow" "7"
+bindkey -s "^[Ox" "8"
+bindkey -s "^[Oy" "9"
+bindkey -s "^[Ol" "+"
+bindkey -s "^[Om" "-"
+bindkey -s "^[Oj" "*"
+bindkey -s "^[Oo" "/"
+' >> ~/.zshrc
 
 curl https://raw.githubusercontent.com/amix/vimrc/master/vimrcs/basic.vim >> ~/.vimrc
 curl https://raw.githubusercontent.com/chenset/vimrc/master/.vimrc >> ~/.vimrc
