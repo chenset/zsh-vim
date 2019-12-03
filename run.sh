de6c085c6aa729b94aebd4e5169fe9d1effe6ee3
chen <4199191@qq.com>
2019-12-03 16:26:41 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
     if [ -f /etc/redhat-release ]; then
-       sudo yum clean all && sudo  yum install zsh git vim -y
+       sudo yum clean all && sudo  yum install zsh git vim lrzsz -y
     fi
 
     if [ -f /etc/lsb-release ]; then
-       sudo apt update && sudo apt install zsh git vim -y
+       sudo apt update && sudo apt install zsh git vim lrzsz -y
     fi
     
 elif [[ "$OSTYPE" == "darwin"* ]]; then
